f80127e35b064588577e5c96ca5b79599204347b
rauloc <raul310388@gmail.com>
2016-11-17 17:54:45 -0500

    primer

---
+++ src/com/entidades/PersonaEL.java
@@ -0,0 +1,70 @@
+package com.entidades;
+
+public class PersonaEL {
+	private int idPersona;
+	//private UsuarioEL Usuario;
+	private String nombres;
+	private String apellidos;
+	private String dniPersona;
+	private String direccion;
+	private String fechaNacimiento;
+	private String Sexo;
+	private String fechaRegistro;
+	private Boolean estado;
+	public int getIdPersona() {
+		return idPersona;
+	}
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDniPersona() {
+		return dniPersona;
+	}
+	public void setDniPersona(String dniPersona) {
+		this.dniPersona = dniPersona;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getSexo() {
+		return Sexo;
+	}
+	public void setSexo(String sexo) {
+		Sexo = sexo;
+	}
+	public String getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(String fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	public Boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+	
+
+}
---
+++ src/com/entidades/UsuarioEL.java
@@ -0,0 +1,54 @@
+package com.entidades;
+
+public class UsuarioEL {
+	private int idUsuario;
+	private TipoEL Tipo;
+	private PersonaEL Persona;
+	private String login;
+	private String password;
+	private boolean estado;
+	private String fechaRegistro;
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public TipoEL getTipo() {
+		return Tipo;
+	}
+	public void setTipo(TipoEL tipo) {
+		Tipo = tipo;
+	}
+	public PersonaEL getPersona() {
+		return Persona;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	public void setPersona(PersonaEL persona) {
+		Persona = persona;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(String fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+}
