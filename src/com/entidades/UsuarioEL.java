6b08922aa073715d2b837dc1d7344a65d3ed5eef
rauloc <raul310388@gmail.com>
2016-11-17 20:46:29 -0500

    qwe

--- src/com/entidades/UsuarioEL.java
+++ src/com/entidades/UsuarioEL.java
@@ -8,7 +8,7 @@ public class UsuarioEL {
 	private String password;
 	private boolean estado;
 	private String fechaRegistro;
-	private String ocola;
+	private String raul;
 	public int getIdUsuario() {
 		return idUsuario;
 	}
