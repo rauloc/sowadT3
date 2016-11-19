ee032304d9670ac1bebcf50c9aa1a150414be0b8
rauloc <raul310388@gmail.com>
2016-11-18 21:35:55 -0500

    asd

---
+++ src/com/entidades/TipoEL.java
@@ -0,0 +1,19 @@
+package com.entidades;
+
+public class TipoEL {
+	private int idTipo;
+	private String descripcion;
+	public int getIdTipo() {
+		return idTipo;
+	}
+	public void setIdTipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
