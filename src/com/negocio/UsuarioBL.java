fcc1a7186787d721309f0309e14779f0c2f84683
walter <walter@Walter>
2016-11-19 12:26:38 -0500

    wrm

---
+++ src/com/negocio/UsuarioBL.java
@@ -0,0 +1,37 @@
+package com.negocio;
+
+import com.accesoadatos.UsuarioDL;
+import com.entidades.UsuarioEL;
+
+public class UsuarioBL {
+	//Singleton
+		public static UsuarioBL _Instancia;
+		private UsuarioBL(){};
+		public static UsuarioBL Instancia(){
+			if(_Instancia==null){
+				_Instancia = new UsuarioBL();
+			}
+			return _Instancia;
+		}
+	//endSingleton
+		public UsuarioEL VerificarAcceso(String _Usuario, String _Pass) 
+				throws Exception{
+			try {
+				UsuarioEL u = UsuarioDL.Instancia().VerificarAcceso(_Usuario, _Pass);
+				if(u==null){
+					throw new ArithmeticException("Usuario o Password no valido!");
+			}else if(!u.isEstado()){
+				throw new ArithmeticException("Su usuario ha sido dado de baja!");
+			}				return u;
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+		public Boolean InsertarUsuario(UsuarioEL p) throws Exception {
+			try {
+				return UsuarioDL.Instancia().InsertarUsuario(p);
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+}
