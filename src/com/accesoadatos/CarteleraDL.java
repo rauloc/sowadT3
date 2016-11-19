3af3b126b5be48c83fb7a55fd7c55e93d3dc1631
rauloc <raul310388@gmail.com>
2016-11-19 06:05:47 -0500

    1er commit dl cartelera

--- src/com/accesoadatos/CarteleraDL.java
+++ src/com/accesoadatos/CarteleraDL.java
@@ -2,4 +2,16 @@ package com.accesoadatos;
 
 public class CarteleraDL {
 
+	// Singleton
+				public static CarteleraDL _Instancia;
+				private CarteleraDL() {
+				};
+				public static CarteleraDL Instancia() {
+					if (_Instancia == null) {
+						_Instancia = new CarteleraDL();
+					}
+					return _Instancia;
+				}
+		// endSingleton
+	
 }
