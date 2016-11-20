da288a9b780051363a8550586c658ec592521a87
walter <walter@Walter>
2016-11-19 20:08:38 -0500

    valida

---
+++ src/com/negocio/UsuarioBL.java
@@ -0,0 +1,34 @@
+package com.negocio;
+
+import com.accesoadatos.UsuarioDL;
+import com.entidades.UsuarioEL;
+
+public class UsuarioBL {
+	//Singleton
+		public static UsuarioBL _Instancia;
+		private UsuarioBL(){};
+		public static UsuarioBL Instancia(){
+			if(_Instancia==null){
+				_Instancia = new UsuarioBL();
+			}
+			return _Instancia;
+		}
+	//endSingleton
+		public UsuarioEL VerificarAcceso(String _Usuario, String _Pass) 
+				throws Exception{
+			try {
+				UsuarioEL u = UsuarioDL.Instancia().VerificarAcceso(_Usuario, _Pass);
+				if(u==null){
+					throw new ArithmeticException("Usuario o Password no valido!");
+				}
+				if(u.getTipo().getIdTipo()!=1){
+					throw new ArithmeticException("Usuario no valido para esta accion");		
+			}else if(!u.isEstado()){
+				throw new ArithmeticException("Su usuario ha sido dado de baja!");
+			}				
+				return u;
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+}
