ff289e450c8c44de62dfa25582744918db3aa4c2
walter <walter@Walter>
2016-11-22 11:42:21 -0500

    update Usuario

--- src/com/negocio/UsuarioBL.java
+++ src/com/negocio/UsuarioBL.java
@@ -14,16 +14,14 @@ public class UsuarioBL {
 			return _Instancia;
 		}
 	//endSingleton
-		public UsuarioEL VerificarAcceso(String _Usuario, String _Pass, int idTipo) 
+		public UsuarioEL VerificarAcceso(String _Usuario, String _Pass) 
 				throws Exception{
 			try {
-				UsuarioEL u = UsuarioDL.Instancia().VerificarAcceso(_Usuario, _Pass,idTipo);
+				UsuarioEL u = UsuarioDL.Instancia().VerificarAcceso(_Usuario, _Pass);
 				if(u==null){
 					throw new ArithmeticException("Usuario o Password no valido!");
 				}
-				if(u.getTipo().getIdTipo()!=1){
-						throw new ArithmeticException("Usuario no valido para esta accion");
-			}else if(!u.isEstado()){
+				else if(!u.isEstado()){
 				throw new ArithmeticException("Su usuario ha sido dado de baja!");
 			}				return u;
 			} catch (Exception e) {
@@ -32,13 +30,13 @@ public class UsuarioBL {
 		}
 
 		//CORREGIDO
-		public Boolean InsertarUsuario(UsuarioEL p) throws Exception {
+	/*	public Boolean InsertarUsuario(UsuarioEL p) throws Exception {
 			try {
 				return UsuarioDL.Instancia().InsertarUsuario(p);
 			} catch (Exception e) {
 				throw e;
 			}
-		}
+		}*/
 
 }
 
