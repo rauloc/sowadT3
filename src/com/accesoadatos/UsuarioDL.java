7e90e25204fb97d836f1ce38f8eb8c637c060d17
walter <walter@Walter>
2016-11-23 00:06:48 -0500

    insertar

--- src/com/accesoadatos/UsuarioDL.java
+++ src/com/accesoadatos/UsuarioDL.java
@@ -47,52 +47,17 @@ public class UsuarioDL {
 				}
 			}
 			
-			/*public UsuarioEL VerificarAcceso(String _Usuario, String _Pass, int idTipo) throws Exception {
-				Connection cn = null;
-				UsuarioEL u = null;
-				try {
-					cn = Conexion.Instancia().Conectar();
-					CallableStatement cst = cn.prepareCall("{call sp_VerificarAcceso(?,?,?)}");
-					cst.setString(1, _Usuario);
-					cst.setString(2, _Pass);
-					cst.setInt(3, idTipo);
-					ResultSet rs = cst.executeQuery();
-					if (rs.next()) {
-						u = new UsuarioEL();
-						u.setIdUsuario(rs.getInt("idUsuario"));
-						u.setLogin(rs.getString("login"));
-						u.setEstado(rs.getBoolean("estado"));
-						TipoEL t=new TipoEL();
-						t.setIdTipo(rs.getInt("idTipo"));
-						u.setTipo(t);
-						PersonaEL p=new PersonaEL();
-						p.setNombres(rs.getString("nombres"));
-						p.setApellidos(rs.getString("apellidos"));
-						p.setDniPersona(rs.getString("dniPersona"));
-						p.setSexo(rs.getString("Sexo"));
-						u.setPersona(p);
-					}
-					return u;
-				} catch (Exception e) {
-					throw e;
-				} finally {
-					cn.close();
-				}
-			}
 			public Boolean InsertarUsuario(UsuarioEL p) throws Exception {
 				Connection cn = null;
 				Boolean inserto = false;
 				try {
 					cn = Conexion.Instancia().Conectar();
-					CallableStatement cst = cn.prepareCall("{call sp_InsertarPersona(?,?,?,?,?,?,?,?)}");
-					cst.setString(1, p.getPersona().getNombres());
-					cst.setString(2, p.getPersona().getApellidos());
-					cst.setString(3, p.getPersona().getDniPersona());
-					cst.setString(4, p.getPersona().getDireccion());
-					cst.setString(5, p.getPersona().getFechaNacimiento());
-					cst.setString(6, p.getPersona().getSexo());
-					cst.setString(7, p.getLogin());
-					cst.setString(8, p.getPassword());
+					CallableStatement cst = cn.prepareCall("{call sp_InsertarUsuario(?,?,?,?,?)}");
+					cst.setString(1, p.getUsuario());
+					cst.setString(2, p.getPassword());
+					cst.setString(3, p.getNombre());
+					cst.setString(4, p.getApellido());
+					cst.setInt(5, p.getDNI());
 					int i = cst.executeUpdate();
 					if(i>0){ inserto=true;}
 				} catch (Exception e) {
@@ -101,5 +66,5 @@ public class UsuarioDL {
 					cn.close();
 				}
 				return inserto;
-			}	*/			
+			}			
 }
