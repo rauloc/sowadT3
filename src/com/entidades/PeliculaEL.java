aa7c3a809c107914abcf46db54fca486cc84b7f6
rauloc <raul310388@gmail.com>
2016-11-24 05:18:11 -0500

    cambios entidad pelicula

--- src/com/entidades/PeliculaEL.java
+++ src/com/entidades/PeliculaEL.java
@@ -1,5 +1,44 @@
 package com.entidades;
 
 public class PeliculaEL {
+	
+	private String Titulo;
+	private String Descripcion;
+	private String Genero;
+	private String Imagen;
+	private boolean Estado;
+	
+	public String getTitulo() {
+		return Titulo;
+	}
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public String getGenero() {
+		return Genero;
+	}
+	public void setGenero(String genero) {
+		Genero = genero;
+	}
+	public String getImagen() {
+		return Imagen;
+	}
+	public void setImagen(String imagen) {
+		Imagen = imagen;
+	}
+	public boolean isEstado() {
+		return Estado;
+	}
+	public void setEstado(boolean estado) {
+		Estado = estado;
+	}
+	
+	
 
 }
