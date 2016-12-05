7d4c4dcf80165d15cc8a6829fffbf828a4f35a46
rauloc <raul310388@gmail.com>
2016-12-05 16:06:42 -0500

    ddddd

--- src/com/accesoadatos/UsuarioDL.java
+++ src/com/accesoadatos/UsuarioDL.java
@@ -3,7 +3,9 @@ package com.accesoadatos;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import com.entidades.PeliculaEL;
 import com.entidades.PersonaEL;
 import com.entidades.TipoEL;
 import com.entidades.UsuarioEL;
@@ -47,7 +49,7 @@ public class UsuarioDL {
 					cn.close();
 				}
 			}
-			
+			//walter insertarUsuario
 			public Boolean InsertarUsuario(UsuarioEL p) throws Exception {
 				Connection cn = null;
 				Boolean inserto = false;
@@ -67,5 +69,51 @@ public class UsuarioDL {
 					cn.close();
 				}
 				return inserto;
-			}			
+			}	
+			
+			public Boolean InsertarUsuarioEmp(UsuarioEL p) throws Exception {
+				Connection cn = null;
+				Boolean inserto = false;
+				try {
+					cn = Conexion.Instancia().Conectar();
+					CallableStatement cst = cn.prepareCall("{call sp_InsertarUsuarioEmp(?,?,?,?,?)}");
+					cst.setString(1, p.getUsuario());
+					cst.setString(2, p.getPassword());
+					cst.setString(3, p.getNombre());
+					cst.setString(4, p.getApellido());
+					cst.setInt(5, p.getDNI());
+					int i = cst.executeUpdate();
+					if(i<0){ inserto=true;}
+				} catch (Exception e) {
+					throw e;
+				} finally {
+					cn.close();
+				}
+				return inserto;
+			}	
+			
+			public ArrayList<UsuarioEL> ListarUsuarios() throws Exception{
+				Connection cn = null;
+				ArrayList<UsuarioEL> lista = new ArrayList<UsuarioEL>();
+				try {
+					cn = Conexion.Instancia().Conectar();
+					CallableStatement cst = cn.prepareCall("{call spListarUsuarios()}");			
+					ResultSet rs = cst.executeQuery();
+					while (rs.next()) {
+						UsuarioEL p = new UsuarioEL();
+						p.setIdUsuario(rs.getInt("IdUsuario"));
+						p.setUsuario(rs.getString("Usuario"));
+						p.setPassword(rs.getString("Password"));
+						p.setNombre(rs.getString("Nombre"));
+						p.setApellido(rs.getString("Apellido"));
+						p.setDNI(rs.getInt("DNI"));
+						lista.add(p);
+					}			
+				} catch (Exception e) {
+					throw e;
+				} finally {
+					cn.close();
+				}
+				return lista;		
+			}
 }
